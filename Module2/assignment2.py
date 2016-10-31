847692cf6b1582f355c895e71c5556bae426aeb2
jomaralexis41 <jomar_alexis41@yahoo.com>
2016-10-31 21:38:53 +0800

    Update assignment2.py

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -3,13 +3,13 @@ import pandas as pd
 # TODO: Load up the 'tutorial.csv' dataset
 #
 # .. your code here ..
-
+df = pd.read_csv('Datasets/tutorial.csv')
 
 
 # TODO: Print the results of the .describe() method
 #
 # .. your code here ..
-
+print df.describe()
 
 
 # TODO: Figure out which indexing method you need to
@@ -17,4 +17,4 @@ import pandas as pd
 # And print the results
 #
 # .. your code here ..
-
+print df.loc[2:4, 'col3']
